5db004703d05c8d3a9c015c863d370fbaa2b9888
Lokesh Surana <Lokesh8203@gmail.com>
2023-01-09 15:08:15 +0530

    Quiz 1 completed

--- Python/Quiz_1/1.py
+++ Python/Quiz_1/1.py
@@ -1,1 +1,13 @@
-import numpy
+#Find the relation between x and y if (x,y), (1,2), and (7,0) are collinear
+
+import numpy as np
+from sympy import symbols, Matrix, simplify
+from sympy.plotting import plot
+x, y = symbols("x, y")
+
+A = np.array([x,y])
+B = np.array([1,2])
+C = np.array([7,0])
+
+eq = simplify(Matrix([B-A, C-A]).det())
+print("The relation between x and y is", eq, "= 0")
