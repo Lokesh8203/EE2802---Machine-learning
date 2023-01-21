42069eb077db98a02f2b9ad72430a836b69e9d7f
Lokesh Surana <Lokesh8203@gmail.com>
2023-01-21 18:20:20 +0530

    11.10.1.7 Final

--- 11.10.1.7/codes/ans.py
+++ 11.10.1.7/codes/ans.py
@@ -1,15 +1,15 @@
 #Find the slope of the line, which makes an angle of 30° with the positive direction of y -axis measured anticlockwise.
 
 import numpy as np
-import sympy as sp
+import matplotlib.pyplot as plt
 
 #direction vector of y axis
 y = np.array([0,1])
 
-#Rotate this direction vector by pi/6 in anticlockwise direction to get the direction vector of the line
+#Rotate this direction vector by (2*pi - pi/6) in anticlockwise direction to get the direction vector of the line
 
 #Rotation matrix
-R = np.array([[np.cos(np.pi/6), -np.sin(np.pi/6)],[np.sin(np.pi/6), np.cos(np.pi/6)]])
+R = np.array([[np.cos(11*np.pi/6), -np.sin(11*np.pi/6)],[np.sin(11*np.pi/6), np.cos(11*np.pi/6)]])
 #direction vector of the line
 l = R@y
 
@@ -17,3 +17,11 @@ l = R@y
 m = l[1]/l[0]
 print("Slope of the line is", m)
 
+#Plot
+x = np.linspace(-1,1,100)
+y = m*x
+plt.plot(x,y)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.savefig('/home/lokesh/EE2802/EE2802-Machine_learning/11.10.1.7/figs/line.png')
