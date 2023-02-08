a6eb7dc6b8ec15190d3f22455adc0dab8bea933d
Lokesh Surana <Lokesh8203@gmail.com>
2023-02-08 09:28:09 +0530

    Rank method

--- 12.10.3.17/codes/ans.py
+++ 12.10.3.17/codes/ans.py
@@ -6,17 +6,20 @@ a = np.array([2,-1,1])
 b = np.array([1,-3,-5])
 c = np.array([3,-4,-4])
 
-#Vectors of sides of triangle
-A = a-b
-B = b-c
-C = c-a
+one_row = np.array([1,1,1])
 
-#area of triangle from above vectors is
-area = 1/2*(np.linalg.norm(np.cross(A,B)))
+#rank of the matrix method to check if points form a triangle
+if np.linalg.matrix_rank(np.array([a,b,c,one_row])) == 3:
+    print("The vectors form a triangle")
+else:
+    print("The vectors do not form a triangle")
 
+A = a - b
+B = b - c
+C = c - a
 
 #Check if the vectors form a right angled triangle
-if (area != 0) and (np.dot(A,B) == 0) or (np.dot(B,C) == 0) or (np.dot(C,A) == 0):
+if (np.dot(A,B) == 0) or (np.dot(B,C) == 0) or (np.dot(C,A) == 0):
     print("The vectors form a right angled triangle")
 else:
     print("The vectors do not form a right angled triangle")
