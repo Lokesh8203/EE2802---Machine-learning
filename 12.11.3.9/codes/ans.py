34edaa159d44ccc101b5c1c033f893fc8a6bf25c
Lokesh Surana <Lokesh8203@gmail.com>
2023-02-09 16:41:36 +0530

    generalized code

--- 12.11.3.9/codes/ans.py
+++ 12.11.3.9/codes/ans.py
@@ -4,15 +4,20 @@ import numpy as np
 import sympy as sp
 
 # Define the planes
-p1 = np.array([3, -1, 2, -4])
-p2 = np.array([1, 1, 1, -2])
+#p = n'x - c
+n1 = np.array([3, -1, 2])
+n2 = np.array([1,1,1])
+c1 = 4
+c2 = 2
 
 lambda_ = sp.symbols('lambda')
-print('Equation of the plane through the intersection of the planes 3x–y + 2z–4 = 0 and x + y + z–2 = 0 and the point (2,2,1) is:', p1 + lambda_*(p2))
 
-equation = (p1 + lambda_*(p2))[0:3]@np.array([2, 2, 1]) + (p1 + lambda_*(p2))[3]
+#given point is x
+x = np.array([2,2,1])
+
+equation = (n1 + lambda_*(n2))@x - (c1 + lambda_*(c2))
 
 lambda_ = sp.solve(equation, lambda_)[0]
 
-normal = (p1 + lambda_*(p2))[0:3]
-print('The equation of plane is {}x + {}y + {}z + {} = 0'.format(normal[0], normal[1], normal[2], (p1 + lambda_*(p2))[3]))
+normal = (n1 + lambda_*(n2))
+print('The equation of plane is {}x + {}y + {}z + {} = 0'.format(normal[0], normal[1], normal[2], -(c1 + lambda_*(c2))))
