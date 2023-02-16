5d33867d0dc3b011e87b53c0093d78aa118ea295
Lokesh Surana <Lokesh8203@gmail.com>
2023-02-16 22:30:11 +0530

    code update 11.11.2.5

--- 11.11.2.5/codes/ans.py
+++ 11.11.2.5/codes/ans.py
@@ -1,42 +1,26 @@
 #Find the coordinates of the focus, axis of the parabola, the equation of the directrix and the length of the latus rectum y^2 = 10x
-
 import numpy as np
 
-def norm(x):
-    return np.sqrt(x[0]*x[0] + x[1]*x[1])
+def focus(c, e, n, u, w):
+    return (c*(e**2)*n - u)/(w[1])
 
-def n(l2, P):
-    return np.sqrt(l2)*P[0]
-
-def c(u, l2, f, n):
-    return (norm(u)*norm(u) - l2*f)/(2*(np.transpose(u))@n)
-
-def focus(c, e, n, u, l2):
-    return (c*e*e*n - u)/(l2)
-
-def l1(V):
-    return V[0][0]
-
-def l2(V):
-    return V[1][1]
-
-def LR(u, P, l2):
-    return (-2*np.transpose(u)@P[0])/(l2)
+def LR(u, v, w):
+    return (-2*(u@v[0]))/(w[1])
 
 V = np.array([[0, 0], [0, 1]])
 u = np.array([-5, 0])
 f = 0
-P = np.array([[1, 0], [0, 1]])
 e = 1 #parabola
 
-lambda_1 = l1(V)
-lambda_2 = l2(V)
+w,v = np.linalg.eig(V)
+#w eigenvalues
+#v eigenvectors
 
-n = n(lambda_2, P)
-c  = c(u, lambda_2, f, n)
+n = np.sqrt(w[1])*v[0]
+c = (np.linalg.norm(u)**2 - w[1]*f)/(2*(u@n))
 
-F = focus(c, e, n, u, lambda_2)
-Latus_rectum = LR(u, P, lambda_2)
+F = focus(c, e, n, u, w)
+Latus_rectum = LR(u, v, w)
 
 print("The focus of parabola is ", F)
 print("The directrix equations is given as", np.transpose(n), 'x = ', c)
