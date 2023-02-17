dddd10cb0769ce91cc345702e635255e2b1bf7cd
Lokesh Surana <Lokesh8203@gmail.com>
2023-02-17 20:53:39 +0530

    11.11.2.5 added figure

--- 11.11.2.5/codes/ans.py
+++ 11.11.2.5/codes/ans.py
@@ -1,5 +1,11 @@
 #Find the coordinates of the focus, axis of the parabola, the equation of the directrix and the length of the latus rectum y^2 = 10x
 import numpy as np
+import matplotlib.pyplot as plt
+
+#Generating points on a parabola
+def parab_gen(y,a):
+	x = y**2/a
+	return x
 
 def focus(c, e, n, u, w):
     return (c*(e**2)*n - u)/(w[1])
@@ -11,6 +17,7 @@ V = np.array([[0, 0], [0, 1]])
 u = np.array([-5, 0])
 f = 0
 e = 1 #parabola
+a = 10
 
 w,v = np.linalg.eig(V)
 #w eigenvalues
@@ -25,5 +32,17 @@ Latus_rectum = LR(u, v, w)
 print("The focus of parabola is ", F)
 print("The directrix equations is given as", np.transpose(n), 'x = ', c)
 print("The lenght of LR of parabola is", Latus_rectum)
+
+#generate parabola plot
+len = 100
+y = np.arange(-10,10,1)
+x = parab_gen(y,a)
+
+plt.plot(x,y,label='$Parabola$')
+plt.plot(F[0], F[1], 'o', label='$Focus$')
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.grid()
+plt.savefig('/home/lokesh/EE2802/EE2802-Machine_learning/11.11.2.5/figs/parabola.png')
 
 
