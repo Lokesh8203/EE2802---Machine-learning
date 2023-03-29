72a62863572a4b1ee44bed19bc429e201654ecc4
Lokesh Surana <Lokesh8203@gmail.com>
2023-03-29 11:50:07 +0530

    Gradient descent method

--- 11.10.3.10/gradient decent/codes/ans.py
+++ 11.10.3.10/gradient decent/codes/ans.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sympy as sp
-
-omat = np.array([[0,1],[-1,0]])
+import math
 
 #Generate line points
+def line_gen_vector(n, c, x):
+    y = (c - n[0]*x)/n[1]
+    return y
+
 def line_gen(A,B):
   len =10
   dim = A.shape[0]
@@ -15,51 +17,72 @@ def line_gen(A,B):
     x_AB[:,i]= temp1.T
   return x_AB
 
-def line_gen_vector(n, c, x):
-    y = (c - n[0]*x)/n[1]
-    return y
+lamda_n = -5 # Start value at lamda_n= -5
+alpha = 0.001 # step size multiplier
+precision = 0.0000001
+previous_step_size = 1 
+max_count = 10000000 # maximum number of iterations
+count = 0 # counter
 
-h = sp.Symbol('h')
+def func_derivative(x):
+    return 260*x - 260/7  # f'(x)
+
+while (previous_step_size > precision) & (count < max_count):
+    lamda_n_minus1 = lamda_n
+    lamda_n -= alpha * func_derivative(lamda_n_minus1)
+    previous_step_size = abs(lamda_n - lamda_n_minus1)
+    count+=1
+
+print("The minimum value of function is at", lamda_n)
+
+def f(x) :
+    return (130*x**2 -(260/7)*x + math.sqrt(36010)/63) #Objective function
+
+x = np.linspace(-1,1,100)#points on the x axis
+y=  f(x) #Objective function
+plt.figure(1)
+plt.plot(x,y, label = '$f(x)$')
+plt.plot([lamda_n],[f(lamda_n)],marker='o',label='$\lambda_{Min}$')
+plt.grid()
+plt.xlabel('$x-Axis$')
+plt.ylabel('$y-Axis$')
+plt.title('Minimum Value of Function')
+plt.legend(loc = 'best')
+plt.savefig('/home/lokesh/EE2802/EE2802-Machine_learning/11.10.3.10/gradient decent/figs/plot.jpg')
+
+plt.close()
 
 #Given points
-A  = np.array([h, 3])
-B = np.array([4, 1])
+O = np.array([22/9, 3])
+A = np.array([19/7, 0])
+
+P = np.array([4, 1])
 
 #Given line
 n = np.array([7, -9])
 c = 19
 
-#Slope of given line
-m = n@omat
-
-#As its given both lines are perpendicular, so
-h = sp.solve(sp.Eq(m@(B-A),0),h)[0]
-print(h)
-
-A = np.array([h, 3]) #Redifining A
-
-#Plot the given line
 x = np.linspace(-7, 12, 100)
 
 #Plot line formed by A and B
-y = line_gen_vector((B-A)@omat, ((B-A)@omat)@A, x)
-plt.plot(x, y, label='$(2  14/9)x = 86/9$')
-
-x = np.linspace(-7, 12, 100)
+x_OP = line_gen(O, P)
+plt.plot(x_OP[0,:],x_OP[1,:],label='$Perpendicular$')
 
 #Plot the given line
+x = np.linspace(-7, 12, 100)
 y = line_gen_vector(n, c, x)
 plt.plot(x, y, label='$(7  -9)x = 19$')
 
 #Plot the points
-plt.plot(A[0], A[1], 'o')
-plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
-plt.plot(B[0], B[1], 'o')
-plt.text(B[0] * (1 + 0.1), B[1] * (1 - 0.1) , 'B')
+plt.plot(O[0], O[1], 'o')
+plt.text(O[0] * (1 + 0.1), O[1] * (1 - 0.1) , 'O')
+plt.plot(P[0], P[1], 'o')
+plt.text(P[0] * (1 + 0.1), P[1] * (1 - 0.1) , 'P')
+
+#legend
 plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.legend(loc='best')
-plt.grid()
+plt.grid() # minor
 plt.axis('equal')
-plt.show()
-# plt.savefig('/home/lokesh/EE2802/EE2802-Machine_learning/11.10.3.10/figs/lines.png')
+plt.savefig('/home/lokesh/EE2802/EE2802-Machine_learning/11.10.3.10/gradient decent/figs/lines.jpg')
