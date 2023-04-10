b6072f63057b5e1dd8a5dc54d59d662ae6c6c4cd
Lokesh Surana <Lokesh8203@gmail.com>
2023-04-10 10:55:22 +0530

    Grad desc

--- 12.6.3.18/codes/ans.py
+++ 12.6.3.18/codes/ans.py
@@ -43,11 +43,25 @@ plt.savefig('/home/lokesh/EE2802/EE2802-Machine_learning/12.6.3.18/figs/rectangl
 plt.close()
 
 def func(x):
-    return 4*(x**3) - 138*(x**2) + 1080*x  # f(x)
+    return 4*(x**3) - 138*(x**2) + 1080*x # f(x)
 
-#Gradient descent
 def func_derivative(x):
-    return 12*(x**2) - 276*x + 1080  # f'(x)
+    return -12*(x**2) + 276*x - 1080  # f'(x)
+
+lamda_n = 0 # Start value at lamda_n= 0
+alpha = 0.001 # step size multiplier
+precision = 0.0000001
+previous_step_size = 1 
+max_count = 10000000 # maximum number of iterations
+count = 0 # counter
+
+while (previous_step_size > precision) & (count < max_count):
+    lamda_n_minus1 = lamda_n
+    lamda_n -= alpha * func_derivative(lamda_n_minus1)
+    previous_step_size = abs(lamda_n - lamda_n_minus1)
+    count+=1
+
+print("The minimum value of function is at", lamda_n)
 
 #Plot the graph of fucn
 x = np.linspace(-1, 13, 100)
